1dfebf3e90e5772898c1cd23697304cf7b3021a8
Tarek <83262890+tarek102@users.noreply.github.com>
2022-08-09 18:35:03 +0200

    Create components and Calculator.js, design the ccalculator

---
+++ src/components/Calculator.js
@@ -0,0 +1,34 @@
+function Calculator() {
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <div className="display">
+          <span>0</span>
+        </div>
+        <div className="symbols">
+          <button className="btn" type="button">AC</button>
+          <button className="btn" type="button">+/-</button>
+          <button className="btn" type="button">%</button>
+          <button className="btn orange" type="button">÷</button>
+          <button className="btn" type="button">7</button>
+          <button className="btn" type="button">8</button>
+          <button className="btn" type="button">9</button>
+          <button className="btn orange" type="button">x</button>
+          <button className="btn" type="button">4</button>
+          <button className="btn" type="button">5</button>
+          <button className="btn" type="button">6</button>
+          <button className="btn orange" type="button">-</button>
+          <button className="btn" type="button">1</button>
+          <button className="btn" type="button">2</button>
+          <button className="btn" type="button">3</button>
+          <button className="btn orange" type="button">+</button>
+          <button className="btn zero" type="button">0</button>
+          <button className="btn" type="button">.</button>
+          <button className="btn orange" type="button">=</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Calculator;
