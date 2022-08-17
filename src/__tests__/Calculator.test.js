aa60507ad4f8fa37141ff73784471fd33d966d10
Tarek <83262890+tarek102@users.noreply.github.com>
2022-08-17 16:32:39 +0200

    test user interaction

--- src/__tests__/Calculator.test.js
+++ src/__tests__/Calculator.test.js
@@ -2,7 +2,21 @@ import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import Calculator from '../components/Calculator';
 
-test('renders correctly', () => {
-  const tree = render(<Calculator />);
-  expect(tree).toMatchSnapshot();
-});
+describe('Test rendering and user interaction', () => {
+  it('renders correctly', () => {
+    const tree = render(<Calculator />);
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('User interaction', () => {
+    render(<Calculator />);
+
+    fireEvent.click(screen.getByRole('button', { name: '8' }));
+    fireEvent.click(screen.getByRole('button', { name: '÷' }));
+    fireEvent.click(screen.getByRole('button', { name: '2' }));
+    fireEvent.click(screen.getByRole('button', { name: '=' }));
+
+    expect(screen.getByText('4')).toBeTruthy();
+  })
+})
+
