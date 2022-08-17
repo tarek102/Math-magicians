486e7c9e08c7744d6215c6edd330ac96fad95f27
bengigenc <genc.bengi@gmail.com>
2022-08-17 17:38:12 +0300

    Add snapshot test for home and quote components

---
+++ src/__tests__/Home.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Home from "../components/Home";
+
+test("renders correctly", () => {
+  const tree = render(<Home />);
+  expect(tree).toMatchSnapshot();
+});
